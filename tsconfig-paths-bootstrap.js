128bcecc8c745b53eedcab42cd2be3f75f3081e7
lvqinbo <22980937@qq.com>
2019-09-26 13:19:46 +0800

    fix paths connot find

--- tsconfig-paths-bootstrap.js
+++ tsconfig-paths-bootstrap.js
@@ -1,9 +1,7 @@
 const tsConfig = require('./tsconfig.json')
 const tsConfigPaths = require('tsconfig-paths')
 
-const cleanup = tsConfigPaths.register({
+tsConfigPaths.register({
 	baseUrl: tsConfig.compilerOptions.outDir,
 	paths: tsConfig.compilerOptions.paths,
 })
-
-cleanup()
