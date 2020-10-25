99a0d543da5a69a60872af9351825fefd85574d5
HelloMan <lvqinbo@163.com>
2020-10-25 16:59:54 +0800

    remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory, Reflector } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from '@app/app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { HttpExceptionFilter } from '@app/common/filters/error.filter'
@@ -33,17 +33,17 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..'))
 
   // swagger
-  const options = new DocumentBuilder()
-    .setTitle('YKPINE')
-    .setDescription('lyz个人博客项目API')
-    .setVersion('1.0')
-    .setBasePath('api')
-    // .addTag('blog')
-    .addBearerAuth()
-    .build()
+  // const options = new DocumentBuilder()
+  //   .setTitle('YKPINE')
+  //   .setDescription('lyz个人博客项目API')
+  //   .setVersion('1.0')
+  //   .setBasePath('api')
+  //   // .addTag('blog')
+  //   .addBearerAuth()
+  //   .build()
 
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('api', app, document)
+  // const document = SwaggerModule.createDocument(app, options)
+  // SwaggerModule.setup('api', app, document)
 
   await app.listen(CONFIG.APP.port)
 }
